3467f3aa2b327936201f5b8d64b0c5358409ffcc
Marcel Maatkamp <m.maatkamp@gmail.com>
2022-10-11 19:51:08 +0200

    ais data added

---
+++ stages/1_schemas/main.tf
@@ -0,0 +1,5 @@
+provider "schemaregistry" {
+    schema_registry_url = "http://localhost:8081"
+    # username            = "<confluent_schema_registry_key>"
+    # password            = "<confluent_schema_registry_password>"
+}

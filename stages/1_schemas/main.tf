b278b6aeb9bf701ebc49698ca5fcd453df7d12e9
Marcel Maatkamp <m.maatkamp@gmail.com>
2022-10-11 21:36:58 +0200

    schemata added

--- stages/1_schemas/main.tf
+++ stages/1_schemas/main.tf
@@ -3,3 +3,10 @@ provider "schemaregistry" {
     # username            = "<confluent_schema_registry_key>"
     # password            = "<confluent_schema_registry_password>"
 }
+
+# avro/avpr/ais_vesseldata.avpr
+resource "schemaregistry_schema" "ais_vesseldata" {
+    for_each = fileset("${path.module}/avro/avsc/", "**/*.avsc")
+    subject =substr(each.value, 0, length(each.value)-4)
+    schema  = file("${path.module}/avro/avsc/${each.value}")
+}

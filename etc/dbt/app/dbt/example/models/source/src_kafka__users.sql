880dab79ef33740369f52c8cbb9cb758dc995c42
Marcel Maatkamp <m.maatkamp@gmail.com>
2022-10-15 14:32:10 +0200

    new files

--- etc/dbt/app/dbt/example/models/source/src_kafka__users.sql
+++ etc/dbt/app/dbt/example/models/source/src_kafka__users.sql
@@ -1,14 +1,8 @@
-{{ config(
-    materialized = 'source',
-    tags = ["source","kafka"]
-) }}
+{{ config(materialized='source') }}
 
-CREATE CONNECTION redpanda_broker FOR KAFKA BROKER {{ "'" ~ var('kafka_broker') ~ "'" }};
-CREATE CONNECTION redpanda_registry FOR CONFLUENT SCHEMA REGISTRY URL {{ "'" ~ var('kafka_schema_registry') ~ "'" }};
-
-CREATE SOURCE current_predictions
-  FROM KAFKA CONNECTION redpanda_broker (TOPIC 'events')
-  FORMAT AVRO
-  USING CONFLUENT SCHEMA REGISTRY CONNECTION redpanda_registry
-  INCLUDE TIMESTAMP as event_timestamp
-  ENVELOPE UPSERT;
+CREATE SOURCE {{ this }}
+FROM KAFKA BROKER {{ "'" ~ var('kafka_broker') ~ "'" }}
+TOPIC 'flight_information'
+FORMAT AVRO USING CONFLUENT SCHEMA REGISTRY {{ "'" ~ var('kafka_schema_registry') ~ "'" }}
+INCLUDE TIMESTAMP as event_timestamp
+ENVELOPE UPSERT;

ad086928a7c7d11f76b9eae62434edb0b831acad
Marcel Maatkamp <m.maatkamp@gmail.com>
2022-10-16 11:14:51 +0200

    ais view added

--- etc/dbt/app/dbt/example/models/sources/aisvesseldata.sql
+++ etc/dbt/app/dbt/example/models/sources/aisvesseldata.sql
@@ -3,4 +3,6 @@ CREATE SOURCE {{ this }}
 FROM KAFKA BROKER {{ "'" ~ var('kafka_broker') ~ "'" }}
 TOPIC 'AisVesselData'
 FORMAT AVRO USING CONFLUENT SCHEMA REGISTRY {{ "'" ~ var('kafka_schema_registry') ~ "'" }}
-INCLUDE TIMESTAMP as event_timestamp;
+  INCLUDE HEADERS, TIMESTAMP as event_timestamp
+  ENVELOPE none;
+ -- WITH (SIZE = '3xsmall');

ca9aecbf93ef0a973377561187ef68712a932833
Marcel Maatkamp <m.maatkamp@gmail.com>
2022-10-17 20:37:15 +0200

    producer added

---
+++ application/producer/java/AisProducer/src/main/java/com/maatkamp/example/materialize/aisproducer/AisProducerApplication.java
@@ -0,0 +1,13 @@
+package com.maatkamp.example.materialize.aisproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AisProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AisProducerApplication.class, args);
+	}
+
+}
---
+++ application/producer/java/AisProducer/src/test/java/com/maatkamp/example/materialize/aisproducer/AisProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maatkamp.example.materialize.aisproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AisProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
